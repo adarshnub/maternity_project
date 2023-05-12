aa8644178c22d5303e77440baf125d1c3e6a7f24
adarshnub <adarshmanjady@gmail.com>
2023-05-12 11:17:54 +0530

    Routing completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import HomePage from "./pages/HomePage";
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path="/" >
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/home' element={<HomePage />} />
+          </Route>
+        </Routes>
     </div>
   );
 }
